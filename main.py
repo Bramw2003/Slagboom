b8645fcd9d687c0671995dc7c41057414fd6bc19
Bram de Smidt <bwdesmidt@gmail.com>
2022-08-12 14:01:18 +0200

    Tell the app it's behind a reverse proxy

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import json
+from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask, request, render_template, url_for, jsonify
 from os import system
 
 app = Flask(__name__)
+
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 @app.route('/open')
 def open_gate():
